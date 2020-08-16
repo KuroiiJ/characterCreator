9c33e1a1ffff4657e066eebb66b3a08dc737a3ef
ChloeJ <chloe.jandsten@gmail.com>
2020-08-15 20:57:09 -0400

    Changed so much! Added in 5E api and backend

--- server/server.js
+++ server/server.js
@@ -3,11 +3,22 @@ const express = require('express');
 const server = express();
 const cors = require('cors');
 
+
+//connecting db
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to database!'));
+
+
 server.use(express.json());
 server.use(cors());
 
-server.get("/", (req, res) => {
-    res.send("Hello World!");
-})
+
+//telling the server how to access the DB and what to do wif it
+const router = require('./characterRoutes');
+server.use('/characterRoutes', router);
+
 
 server.listen(3000, () => console.log("Server started!"));
